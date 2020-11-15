3df67bb8e7ce7cf03d4e342f8f89320ef94bfa6d
AdarshGabani <adarshgabani4135@gmail.com>
2020-11-15 13:26:33 +0530

    initial styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
+import React from 'react';
+// Importing Styles
+
+import './styles/app.scss'
+
+// Importing Components
+import Player from './components/Player';
+import Song from './components/Song'
+
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Song />
+      <Player />
     </div>
   );
 }
---
+++ src/components/Song.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Song = () => {
+    return (
+        <div className="song-container">
+            <h1>Song Picture</h1>
+            <h1>Song name</h1>
+            <h1>artist name</h1>
+        </div>
+    )
+}
+
+export default Song;
